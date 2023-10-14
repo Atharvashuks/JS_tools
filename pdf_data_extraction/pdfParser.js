77af217436382afdd01f7f2a75a8add14eeb7edd
Atharvashuks <atharvashuklaas@gmail.com>
2023-10-14 22:27:01 +0530

    pdf extraction tool added

---
+++ pdf_data_extraction/pdfParser.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const pdf = require("pdf-parse");
+
+async function extractTextFromPDF(pdfFilePath) {
+  const dataBuffer = fs.readFileSync(pdfFilePath);
+  const data = await pdf(dataBuffer);
+
+  return data.text;
+}
+
+const pdfFilePath = "newResume P.pdf";
+
+extractTextFromPDF(pdfFilePath)
+  .then((data) => {
+    // console.log(data);
+    const filePath = "PDF_DATA.txt";
+
+    fs.writeFile(filePath, data, (err) => {
+      if (err) {
+        console.error("Error writing JSON data to file:", err);
+      } else {
+        console.log("JSON data has been written to", filePath);
+      }
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
